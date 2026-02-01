d6e710c70283e92feb9373eb196327350f8c3bc5
yarikrazor-star <yarik.razor@rambler.ru>
2026-02-01 18:01:50 +0200

    Update torrqua.js

--- torrqua.js
+++ torrqua.js
@@ -26,7 +26,7 @@
     var JACRED_PROTOCOL = 'https://';
     var JACRED_URL = Lampa.Storage.get('jacred.xyz') || 'jacred.xyz';
     var PROXY_LIST = [
-        'https://well-informed-normal-function.anvil.app/_/api/jackett_proxy?u='
+        'https://well-informed-normal-function.anvil.app/_/api/jackett_proxy?u=',
         'https://my-finder.kozak-bohdan.workers.dev/?url=',
         'http://api.allorigins.win/raw?url=',
         'http://cors.bwa.workers.dev/'
