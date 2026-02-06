5e5c238cae1681149689c22a43679fca981a7983
yarikrazor-star <yarik.razor@rambler.ru>
2026-02-06 04:20:48 +0200

    Add files via upload

---
+++ WikiFind.js
@@ -0,0 +1,167 @@
+(function () {
+    'use strict';
+
+    function UnifiedWikiPlugin() {
+        var ICON_WIKI = 'https://upload.wikimedia.org/wikipedia/commons/b/b3/Wikipedia-logo-v2-en.svg';
+        
+        this.init = function () {
+            var self = this;
+            Lampa.Listener.follow('full', function (e) {
+                if (e.type === 'complite') {
+                    setTimeout(function() {
+                        try {
+                            if (e.object && e.object.activity && e.object.activity.render) {
+                                self.process(e.data.movie, e.object.activity.render());
+                            }
+                        } catch (err) {
+                            console.log('Wiki Error:', err);
+                        }
+                    }, 400);
+                }
+            });
+        };
+
+        this.process = function (movie, render) {
+            if (!movie || !render) return;
+
+            var $render = $(render);
+            $render.find('.surs_wiki_unified').remove();
+
+            var studioRow = $('<div class="surs_wiki_unified surs_studio_row" style="width: 100%; display: block; margin: 0.4em 0; clear: both;">' +
+                                '<div class="wiki_studio_info" style="font-size: 1.2em; color: #fff; opacity: 0.9;">' +
+                                    '<span>Виробник: <span class="studio_list">Пошук...</span></span>' +
+                                '</div>' +
+                            '</div>');
+
+            var wikiBtnRow = $('<div class="surs_wiki_unified surs_wiki_btn_row" style="display: inline-block; vertical-align: middle; margin-left: 8px;">' +
+                                '<div class="surs_wiki_box" style="display: inline-flex; align-items: center; background: transparent; padding: 4px 10px; border-radius: 4px; border: 1px solid rgba(255, 255, 255, 0.5); font-size: 1.1em; color: #fff; cursor: pointer;">' +
+                                    '<img src="' + ICON_WIKI + '" style="width: 1.1em; height: 1.1em; filter: invert(1); opacity: 0.8;">' +
+                                    '<span class="wiki_text" style="margin-left: 6px; font-weight: bold; font-size: 0.85em; text-transform: uppercase;">...</span>' +
+                                '</div>' +
+                               '</div>');
+
+            var slogan = $render.find('.full-start__slogan');
+            var ratings = $render.find('.full-start-new__rate-line, .full-start__rate-line');
+            var age = ratings.find('.full-start__age, .full-start__view');
+
+            if (slogan.length) slogan.after(studioRow);
+            else if (ratings.length) ratings.before(studioRow);
+            else $render.find('.full-start__info').prepend(studioRow);
+
+            if (age.length) age.before(wikiBtnRow);
+            else if (ratings.length) ratings.append(wikiBtnRow);
+
+            this.search(movie, wikiBtnRow, studioRow);
+        };
+
+        this.search = function (movie, btnRow, studioRow) {
+            var self = this;
+            var wikiText = btnRow.find('.wiki_text');
+            var studioList = studioRow.find('.studio_list');
+            
+            var titleEN = (movie.original_title || movie.original_name || movie.title || movie.name || '').replace(/[^\w\s]/gi, '');
+            var year = (movie.release_date || movie.first_air_date || '').substring(0, 4);
+
+            var category = 'film';
+            if (movie.number_of_seasons || movie.first_air_date || (movie.name && !movie.title)) {
+                category = 'television series';
+            }
+            
+            var isAnim = false;
+            if (movie.genre_ids && movie.genre_ids.indexOf(16) !== -1) isAnim = true;
+            if (movie.genres && Array.isArray(movie.genres)) {
+                isAnim = movie.genres.some(function(g) { return g.id === 16; });
+            }
+            if (isAnim) category = 'animated ' + category;
+
+            var queryStr = (titleEN + ' ' + year + ' ' + category).trim();
+
+            $.ajax({
+                url: 'https://en.wikipedia.org/w/api.php',
+                data: { action: 'query', list: 'search', srsearch: queryStr, format: 'json', origin: '*' },
+                dataType: 'json',
+                success: function(res) {
+                    var pageEN = (res.query && res.query.search && res.query.search[0]) ? res.query.search[0] : null;
+
+                    if (pageEN) {
+                        $.ajax({
+                            url: 'https://en.wikipedia.org/w/api.php',
+                            data: { action: 'query', prop: 'langlinks', lllang: 'uk', pageids: pageEN.pageid, format: 'json', origin: '*' },
+                            dataType: 'json',
+                            success: function(details) {
+                                var pageData = (details.query && details.query.pages && details.query.pages[pageEN.pageid]) ? details.query.pages[pageEN.pageid] : {};
+                                var uaTitle = (pageData.langlinks && pageData.langlinks[0]) ? pageData.langlinks[0]['*'] : null;
+                                wikiText.text(uaTitle ? 'UA' : 'EN');
+
+                                btnRow.find('.surs_wiki_box').off('click').on('click', function() {
+                                    var url = uaTitle 
+                                        ? 'https://uk.m.wikipedia.org/wiki/' + encodeURIComponent(uaTitle)
+                                        : 'https://en.m.wikipedia.org/?curid=' + pageEN.pageid;
+                                    self.open(url, movie.title || movie.name);
+                                });
+                            }
+                        });
+
+                        $.ajax({
+                            url: 'https://en.wikipedia.org/w/api.php',
+                            data: { action: 'parse', pageid: pageEN.pageid, prop: 'text', section: 0, format: 'json', origin: '*' },
+                            dataType: 'json',
+                            success: function(data) {
+                                if (data.parse && data.parse.text) {
+                                    var html = data.parse.text['*'];
+                                    var parser = new DOMParser();
+                                    var doc = parser.parseFromString(html, 'text/html');
+                                    var rows = doc.querySelectorAll('.infobox tr');
+                                    var company = "";
+
+                                    for (var i = 0; i < rows.length; i++) {
+                                        var label = rows[i].querySelector('.infobox-label');
+                                        if (label) {
+                                            var txt = label.textContent.toLowerCase();
+                                            if ((txt.indexOf('company') > -1 || txt.indexOf('production') > -1) && txt.indexOf('location') === -1) {
+                                                var dataCell = rows[i].querySelector('.infobox-data');
+                                                if (dataCell) {
+                                                    company = dataCell.textContent.replace(/\[\d+\]/g, '').trim();
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    studioList.text(company || 'Не вказано');
+                                }
+                            }
+                        });
+                    } else {
+                        studioList.text('Не знайдено');
+                        wikiText.text('?');
+                    }
+                },
+                error: function() {
+                    studioList.text('Помилка');
+                    wikiText.text('!');
+                }
+            });
+        };
+
+        this.open = function (url, title) {
+            var enabled = Lampa.Controller.enabled().name;
+            Lampa.Modal.open({
+                title: title,
+                html: $('<div style="height: 500px;"><iframe src="' + url + '" style="width: 100%; height: 100%; border: none; background: #fff; border-radius: 8px;"></iframe></div>'),
+                size: 'large',
+                onBack: function() {
+                    Lampa.Modal.close();
+                    Lampa.Controller.toggle(enabled);
+                }
+            });
+        };
+    }
+
+    if (window.Lampa) {
+        new UnifiedWikiPlugin().init();
+    } else {
+        $(document).on('lampa:ready', function() { 
+            new UnifiedWikiPlugin().init(); 
+        });
+    }
+})();
