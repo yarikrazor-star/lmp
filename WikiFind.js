a3bc6fdd87cfff86addad862acd41d5017c248d2
yarikrazor-star <yarik.razor@rambler.ru>
2026-02-08 01:58:26 +0200

    Rename WikiOnlyPlugin to WikiSmartPlugin

--- WikiFind.js
+++ WikiFind.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    function WikiOnlyPlugin() {
+    function WikiSmartPlugin() {
         var ICON_WIKI = 'https://yarikrazor-star.github.io/lmp/wiki.svg';
         
         this.init = function () {
@@ -11,117 +11,128 @@
                     setTimeout(function() {
                         try {
                             if (e.object && e.object.activity && e.object.activity.render) {
-                                self.process(e.data.movie, e.object.activity.render());
+                                self.render(e.data.movie, e.object.activity.render());
                             }
                         } catch (err) {
-                            console.log('Wiki Plugin Error:', err);
+                            console.log('Wiki Error:', err);
                         }
-                    }, 400);
+                    }, 500);
                 }
             });
         };
 
-        this.process = function (movie, render) {
-            if (!movie || !render) return;
+        this.render = function (movie, html) {
+            var container = $(html);
+            container.find('.lampa-wiki-button').remove();
 
-            var $render = $(render);
-            $render.find('.lampa-wiki-button').remove();
-
-            // Створення кнопки з вирівнюванням по центру
-            var wikiBtn = $('<div class="full-start__button selector lampa-wiki-button" style="display: flex; align-items: center; justify-content: center;">' +
-                                '<img src="' + ICON_WIKI + '" style="width: 1.1em; height: 1.1em; margin-right: 8px; vertical-align: middle;">' +
-                                '<span class="wiki_text">WIKI: Пошук...</span>' +
+            var button = $('<div class="full-start__button selector lampa-wiki-button" style="display: flex; align-items: center; justify-content: center; width: 2.8em; height: 2.8em; padding: 0; min-width: 2.8em;">' +
+                                '<img src="' + ICON_WIKI + '" style="width: 100%; height: 100%; object-fit: contain;">' +
                             '</div>');
 
-            var buttonsContainer = $render.find('.full-start-new__buttons, .full-start__buttons');
-
-            if (buttonsContainer.length) {
-                buttonsContainer.append(wikiBtn);
-                
-                if (Lampa.Activity.active().activity.toggle) {
-                    Lampa.Activity.active().activity.toggle();
-                }
+            var footer = container.find('.full-start-new__buttons, .full-start__buttons');
+            if (footer.length) {
+                footer.append(button);
+                try {
+                    if (Lampa.Activity.active().activity.toggle) Lampa.Activity.active().activity.toggle();
+                } catch(e) {}
             }
 
-            this.search(movie, wikiBtn);
+            this.prepareSearch(movie, button);
         };
 
-        this.search = function (movie, wikiBtn) {
+        this.prepareSearch = function (movie, button) {
             var self = this;
-            var wikiText = wikiBtn.find('.wiki_text');
             
-            var titleEN = (movie.original_title || movie.original_name || movie.title || movie.name || '').replace(/[^\w\s]/gi, '');
+            // Визначаємо тип для уточнення в дужках
+            var isTV = (movie.number_of_seasons || movie.first_air_date || (movie.name && !movie.title));
+            var typeSpec = isTV ? '(TV series)' : '(film)';
+            
+            // Очищаємо назву (тільки латиниця та цифри)
+            var title = (movie.original_title || movie.original_name || '').replace(/[^\w\s]/gi, '');
             var year = (movie.release_date || movie.first_air_date || '').substring(0, 4);
-
-            var category = 'film';
-            if (movie.number_of_seasons || movie.first_air_date || (movie.name && !movie.title)) {
-                category = 'television series';
-            }
             
-            var isAnim = false;
-            if (movie.genre_ids && movie.genre_ids.indexOf(16) !== -1) isAnim = true;
-            if (movie.genres && Array.isArray(movie.genres)) {
-                isAnim = movie.genres.some(function(g) { return g.id === 16; });
-            }
-            if (isAnim) category = 'animated ' + category;
-
-            var queryStr = (titleEN + ' ' + year + ' ' + category).trim();
-
+            // Чіткий шаблон: "Назва (film/TV series) рік"
+            // Це виключає акторів, бо у них в дужках написано (actor)
+            var query = title + ' ' + typeSpec + ' ' + year;
+            
             $.ajax({
                 url: 'https://en.wikipedia.org/w/api.php',
-                data: { action: 'query', list: 'search', srsearch: queryStr, format: 'json', origin: '*' },
+                data: {
+                    action: 'query',
+                    list: 'search',
+                    srsearch: query,
+                    srlimit: 1,
+                    format: 'json',
+                    origin: '*'
+                },
                 dataType: 'json',
-                success: function(res) {
-                    var pageEN = (res.query && res.query.search && res.query.search[0]) ? res.query.search[0] : null;
-
-                    if (pageEN) {
-                        $.ajax({
-                            url: 'https://en.wikipedia.org/w/api.php',
-                            data: { action: 'query', prop: 'langlinks', lllang: 'uk', pageids: pageEN.pageid, format: 'json', origin: '*' },
-                            dataType: 'json',
-                            success: function(details) {
-                                var pageData = (details.query && details.query.pages && details.query.pages[pageEN.pageid]) ? details.query.pages[pageEN.pageid] : {};
-                                var uaTitle = (pageData.langlinks && pageData.langlinks[0]) ? pageData.langlinks[0]['*'] : null;
-                                
-                                wikiText.text(uaTitle ? 'WIKI: UA' : 'WIKI: EN');
-
-                                wikiBtn.off('hover:enter').on('hover:enter', function() {
-                                    var url = uaTitle 
-                                        ? 'https://uk.m.wikipedia.org/wiki/' + encodeURIComponent(uaTitle)
-                                        : 'https://en.m.wikipedia.org/?curid=' + pageEN.pageid;
-                                    self.open(url, movie.title || movie.name);
-                                });
-                            }
+                success: function (res) {
+                    var page = (res.query && res.query.search && res.query.search.length > 0) ? res.query.search[0] : null;
+                    if (page) {
+                        self.findUK(page.title, function (ukLink) {
+                            var finalUrl = ukLink || ('https://en.m.wikipedia.org/wiki/' + encodeURIComponent(page.title));
+                            button.off('hover:enter').on('hover:enter', function () {
+                                self.open(finalUrl, movie.title || movie.name);
+                            });
                         });
                     } else {
-                        wikiText.text('WIKI: ?');
+                        button.css('opacity', '0.3');
                     }
                 },
-                error: function() {
-                    wikiText.text('WIKI: !');
+                error: function () {
+                    button.css('opacity', '0.3');
                 }
+            });
+        };
+
+        this.findUK = function (title, callback) {
+            $.ajax({
+                url: 'https://en.wikipedia.org/w/api.php',
+                data: {
+                    action: 'query',
+                    titles: title,
+                    prop: 'langlinks',
+                    lllang: 'uk',
+                    format: 'json',
+                    origin: '*'
+                },
+                dataType: 'json',
+                success: function (res) {
+                    try {
+                        var pages = res.query.pages;
+                        var id = Object.keys(pages)[0];
+                        if (pages[id].langlinks && pages[id].langlinks[0]) {
+                            var ukTitle = pages[id].langlinks[0]['*'];
+                            callback('https://uk.m.wikipedia.org/wiki/' + encodeURIComponent(ukTitle));
+                        } else {
+                            callback(null);
+                        }
+                    } catch (e) { callback(null); }
+                },
+                error: function () { callback(null); }
             });
         };
 
         this.open = function (url, title) {
-            var enabled = Lampa.Controller.enabled().name;
+            var currentController = Lampa.Controller.enabled().name;
             Lampa.Modal.open({
                 title: title,
                 html: $('<div style="height: 500px;"><iframe src="' + url + '" style="width: 100%; height: 100%; border: none; background: #fff; border-radius: 8px;"></iframe></div>'),
                 size: 'large',
-                onBack: function() {
+                onBack: function () {
                     Lampa.Modal.close();
-                    Lampa.Controller.toggle(enabled);
+                    Lampa.Controller.toggle(currentController);
                 }
             });
         };
     }
 
+    // Запуск без конфліктів
     if (window.Lampa) {
-        new WikiOnlyPlugin().init();
+        new WikiSmartPlugin().init();
     } else {
-        $(document).on('lampa:ready', function() { 
-            new WikiOnlyPlugin().init(); 
+        $(document).off('lampa:ready').on('lampa:ready', function () {
+            new WikiSmartPlugin().init();
         });
     }
 })();
